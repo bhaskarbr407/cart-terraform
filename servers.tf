a9f054fe8d1aa98fcb437bce5c74b7a150861fad
Durga <bhaskarbr407@gmail.com>
2024-02-25 21:13:57 +0530

    servers terraform

--- servers.tf
+++ servers.tf
@@ -4,13 +4,60 @@ data "aws_security_group" "allow-all" {
 variable "instance_type" {
   default = "t3.micro"
 }
+
+variable "components" {
+  default = {
+    frontend = {
+      name = "frontend"
+      instance_type = "t3.micro"
+    }
+    mongodb = {
+      name = "mongodb"
+      instance_type = "t3.micro"
+    }
+    catalogue = {
+      name = "catalogue"
+      instance_type = "t3.micro"
+    }
+    redis ={
+      name = "redis"
+      instance_type = "t3.micro"
+    }
+    user = {
+      name = "user"
+      instance_type = "t3.micro"
+    }
+    cart = {
+      name = "cart"
+      instance_type = "t3.micro"
+    }
+    mysql = {
+      name = "mysql"
+      instance_type = "t3.micro"
+    }
+    shipping = {
+      name = "shipping"
+      instance_type = "t3.micro"
+    }
+    rabbitmq = {
+      name = "rabbitmq"
+      instance_type = "t3.micro"
+    }
+    payment = {
+      name = "payment"
+      instance_type = "t3.small"
+    }
+  }
+}
+
 resource "aws_instance" "frontend" {
+  for_each = var.components
   ami           = data.aws_ami.centos.image_id
-  instance_type = var.instance_type
+  instance_type = each.value["instance_type"]
   vpc_security_group_ids = [data.aws_security_group.allow-all.id]
 
   tags = {
-    Name = "frontend"
+    Name = each.value["name"]
   }
 }
 
