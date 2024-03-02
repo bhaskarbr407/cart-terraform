5d16bce1b4e61b9e34b845c9e7c549b52b7f348b
Durga <bhaskarbr407@gmail.com>
2024-03-02 12:43:45 +0530

    functions terraform

--- servers.tf
+++ servers.tf
@@ -2,7 +2,7 @@ variable "instance_type" {
   default = "t3.micro"
 }
 
-resource "aws_instance" "frontend" {
+resource "aws_instance" "instance" {
   for_each = var.components
   ami           = data.aws_ami.centos.image_id
   instance_type = each.value["instance_type"]
@@ -12,6 +12,27 @@ resource "aws_instance" "frontend" {
     Name = each.value["name"]
   }
 }
+
+resource "null_resource" "provisioner" {
+  depends_on = [ aws_instance.instance ]
+  for_each = var.components
+  provisioner "remote-exec" {
+    connection {
+      type = "ssh"
+      user = "centos"
+      password = "DevOps321"
+      host = aws_instance.instance[each.value["name"]].private_ip
+    }
+
+    inline = [
+      "rm -rf roboshop-shell",
+      "git clone https://github.com/raghudevopsb72/roboshop-shell.git",
+      "cd roboshop-shell",
+      "sudo bash ${each.value["name"]}.sh ${lookup(each.value, "password", "null" )}"
+    ]
+  }
+}
+
 output "ami"{
   value = data.aws_ami.centos.image_id
 }
