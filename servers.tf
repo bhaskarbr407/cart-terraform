f49c89c4be654e227b5f201cbb855bdb3119bbcb
Durga <bhaskarbr407@gmail.com>
2024-03-16 01:36:51 +0530

    terraform

--- servers.tf
+++ servers.tf
@@ -13,25 +13,25 @@ resource "aws_instance" "instance" {
   }
 }
 
-resource "null_resource" "provisioner" {
-  depends_on = [ aws_instance.instance ]
-  for_each = var.components
-  provisioner "remote-exec" {
-    connection {
-      type = "ssh"
-      user = "centos"
-      password = "DevOps321"
-      host = aws_instance.instance[each.value["name"]].private_ip
-    }
+#resource "null_resource" "provisioner" {
+#  depends_on = [ aws_instance.instance ]
+#  for_each = var.components
+#  provisioner "remote-exec" {
+#    connection {
+#      type = "ssh"
+#      user = "centos"
+#      password = "DevOps321"
+#      host = aws_instance.instance[each.value["name"]].private_ip
+#    }
 
-    inline = [
-      "rm -rf roboshop-shell",
-      "git clone https://github.com/raghudevopsb72/roboshop-shell.git",
-      "cd roboshop-shell",
-      "sudo bash ${each.value["name"]}.sh ${lookup(each.value, "password", "null" )}"
-    ]
-  }
-}
+#    inline = [
+#      "rm -rf roboshop-shell",
+#      "git clone https://github.com/raghudevopsb72/roboshop-shell.git",
+#      "cd roboshop-shell",
+#      "sudo bash ${each.value["name"]}.sh ${lookup(each.value, "password", "null" )}"
+#    ]
+#  }
+#}
 
 output "ami"{
   value = data.aws_ami.centos.image_id
