d147033364b022fcf5fee6f20e0d8b444f82ba1a
Durga <bhaskarbr407@gmail.com>
2024-07-19 20:40:18 +0530

    roboshop

--- servers.tf
+++ servers.tf
@@ -3,9 +3,9 @@ variable "instance_type" {
 }
 
 resource "aws_instance" "instance" {
-  for_each = var.components
-  ami           = data.aws_ami.centos.image_id
-  instance_type = each.value["instance_type"]
+  for_each               = var.components
+  ami                    = data.aws_ami.centos.image_id
+  instance_type          = each.value["instance_type"]
   vpc_security_group_ids = [data.aws_security_group.allow-all.id]
 
   tags = {
